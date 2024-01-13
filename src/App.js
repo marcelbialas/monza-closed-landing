b4640a44451dc21e41cd1d32ac598fa65c5eea00
Marcel Bialas <marcelbialas@gmail.com>
2024-01-13 10:32:47 +0100

    changed layout and text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./monza_logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h2>Wir haben geschlossen!</h2>
+          <p>
+            Nach über 27 Jahren im Kartsport haben wir am 18.12.2023 unseren
+            Betrieb eingestellt. <br />
+            <br />
+            Wir möchten uns herzlich bei allen Kund:innen, Mitarbeiter:innen und
+            Freunden für die zahlreichen spannenden Rennen und die
+            unvergesslichen Momente bedanken, die wir gemeinsam erlebt haben.
+            Diese 27 Jahre waren für uns eine großartige Reise, und es war uns
+            eine Ehre, sie mit euch zu teilen.
+          </p>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mit herzlichen Grüßen
+          <br />
+          Team Monza
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
